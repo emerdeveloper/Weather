62829b36a77541b5bdbf7c172d3f3aa63e5c6ed7
Emerson Gonzalez <emersonjavid97@gmail.com>
2020-05-14 10:09:48 -0500

    Initial Commit

---
+++ Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Emerson Javid Gonzalez Morales on 14/05/20.
+//  Copyright © 2020 Emerson Javid Gonzalez Morales. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
